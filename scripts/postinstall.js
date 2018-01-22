1091fab99f13cc432a78b83348a4f2d4c4fca601
deadivan <deadivan@gmail.com>
2018-01-22 11:08:15 +0800

    fix postinstall error

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,3 +1,4 @@
 var exec = require('child_process').exec;
-process.chdir('../ios/Vendor')
+console.log( __dirname )
+process.chdir(__dirname + '/../ios/Vendor')
 exec('git clone https://github.com/rs/SDWebImage.git')
