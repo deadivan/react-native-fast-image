273e36d07003b5591ee5bdd47d11ca37007fcab7
deadivan <deadivan@gmail.com>
2018-01-22 12:21:06 +0800

    fixing postinstall

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,4 +1,7 @@
 var exec = require('child_process').exec;
 console.log( __dirname )
 process.chdir(__dirname + '/../ios/Vendor')
-exec('git clone https://github.com/rs/SDWebImage.git')
+exec('git clone https://github.com/rs/SDWebImage.git', function(){
+	process.chdir(__dirname + '/../')
+	exec('git submodule update --init --recursive')
+})
